c6a9b71740cd6bd75e65904f87d24577cfb4cc83
Param2007 <68640895+Param2007@users.noreply.github.com>
2020-08-13 21:52:18 +0530

    Add files via upload

---
+++ landingArea.js
@@ -0,0 +1,34 @@
+class LandingArea {
+
+    constructor(x,y,width,height) {
+
+        var options = {
+            isStatic: true
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+
+        this.width = width;
+        this.height = height;
+
+        World.add(world, this.body);
+
+    }
+
+    display() {
+
+        push();
+
+        var p = this.body.position;
+
+        //translate(p.x, p.y);
+
+        fill("red");
+        rectMode(CENTER)
+        rect(p.x, p.y, this.width, this.height);
+
+        pop();
+
+    }
+
+}
